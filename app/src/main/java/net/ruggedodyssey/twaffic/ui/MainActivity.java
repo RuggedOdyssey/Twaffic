53b6d8def07baaacf6c7896c5dc434d332046333
Daneel <daneelsmuts@gmail.com>
2014-08-15 11:36:36 +0200

    notification stuff

--- app/src/main/java/net/ruggedodyssey/twaffic/ui/MainActivity.java
+++ app/src/main/java/net/ruggedodyssey/twaffic/ui/MainActivity.java
@@ -1,10 +1,14 @@
 package net.ruggedodyssey.twaffic.ui;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -48,10 +52,49 @@ public class MainActivity extends Activity {
             //((TextView) fragment_obj.getView().findViewById(R.id.textview)).setText(message);
             //fragment_obj.setText(message);
             textFragment.setText(message);
+            createNotification("Traffic Update", message);
 
         }
     };
 
+
+    public void createNotification(String statusText, String contentText) {
+//        Intent mainIntent = new Intent(this, FragmentSettings.class);
+//        mainIntent.setAction("android.intent.action.VIEW)");
+//        mainIntent.putExtra("Action", "Preferences");
+//
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addNextIntent(mainIntent);
+//
+//        PendingIntent pMainIntent = stackBuilder.getPendingIntent(
+//                0,
+//                PendingIntent.FLAG_UPDATE_CURRENT
+//        );
+
+        try {
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.jeep256small);
+            // Build notification
+            Notification notification = new Notification.Builder(this)
+                    .setContentTitle(statusText)
+                            //TODO: Refer to http://developer.android.com/guide/topics/ui/notifiers/notifications.html#HandlingNotifications
+                    .setContentText(contentText)
+                            //.setContentInfo(contentText)
+                    .setTicker(statusText)  //This creates ticker text on every notification refresh, so not using it anymore.
+//                    .setContentIntent(pMainIntent)
+                    .setSmallIcon(R.drawable.jeep24)
+                    .setLargeIcon(bm)
+                    .setWhen(System.currentTimeMillis())
+                    .build();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            // Hide the notification after its selected
+            //notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+            notificationManager.notify(0, notification);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
