54b36c58c5b589044ec84bc27053374f3f3d6808
Daneel <daneelsmuts@gmail.com>
2014-08-15 11:31:29 +0200

    notification stuff

--- backend/src/main/java/net/ruggedodyssey/backend/service/TwitterAPI.java
+++ backend/src/main/java/net/ruggedodyssey/backend/service/TwitterAPI.java
@@ -22,11 +22,8 @@ public class TwitterAPI {
 
     public static String getTestTweet() {
         String tweet = "";
-        int i = 0;
         try {
-            i = 1;
             String user = "Capetownfreeway";
-            //System.out.println("|START|");
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setDebugEnabled(true)
                     .setOAuthConsumerKey(Constants.oauth_consumerKey)
@@ -34,23 +31,18 @@ public class TwitterAPI {
                     .setOAuthAccessToken(Constants.oauth_accessToken)
                     .setOAuthAccessTokenSecret(Constants.oauth_accessTokenSecret);
             Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-            i = 2;
             List<Status> statuses = twitter.getUserTimeline(user);
 
-            //System.out.println("Showing @" + user + "'s home timeline.");
-            i = 3;
             for (Status status : statuses) {
                 tweet = status.getText();
             }
-            i = 4;
 
         } catch (TwitterException te) {
             te.printStackTrace();
             tweet = te.getMessage();
-            //System.out.println("Failed to get timeline: " + te.getMessage());
-            //System.exit(-1);
+
         }
-        return tweet + "["+ String.valueOf(i)+ "]";
+        return tweet;
     }
 
 }
